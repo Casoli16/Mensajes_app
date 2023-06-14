8829d36bfcf01de34f954aa661a5cb52f1d7a21b
Susan Castillo <susancasoli@gmail.com>
2023-06-13 19:52:32 -0600

    Se agrego la funcion para leer mensajes

--- src/main/java/mensajesService.java
+++ src/main/java/mensajesService.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class mensajesService {
+import java.util.Scanner;
+
+public class mensajesService {
+
+    public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
+
+        System.out.println("Tu nombre:");
+        String nombre = sc.nextLine();
+
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        MensajesDAO.crearMensajeDB(registro);
+    }
+
+    public static void listarMensajes(){
+        MensajesDAO.leerMensajeDB();
+    }
+
+    public static void borrarMensaje(){
+
+    }
+
+    public static  void editarMensaje(){
+
+    }
 }
